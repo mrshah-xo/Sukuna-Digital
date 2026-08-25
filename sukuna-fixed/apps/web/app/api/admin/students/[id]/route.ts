33dcc51a65272c058e63845fe928d7a79ae2a6e3
Ayush Shah <ayushshah8082@gmail.com>
2026-08-25 17:16:21 +0545

    Update route.ts

--- sukuna-fixed/apps/web/app/api/admin/students/[id]/route.ts
+++ sukuna-fixed/apps/web/app/api/admin/students/[id]/route.ts
@@ -5,6 +5,8 @@ import connectDB from '@/lib/mongodb';
 import { User, Student, type IUser } from '@/models';
 import { updateStudentSchema, type UpdateStudentInput } from '@/lib/admin-schemas';
 
+export const dynamic = 'force-dynamic';
+
 export const GET = apiHandler(async (req, { user, params }) => {
   await connectDB();
   const { id } = params;
